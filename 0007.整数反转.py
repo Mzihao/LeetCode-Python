e832d28330182a6cdf7b55231e67e85e2ad2bc8f
Mai_Zihao <pumpkin_mak@163.com>
2022-04-06 12:08:33 +0800

    Update 0007.整数反转.py

--- 0007.整数反转.py
+++ 0007.整数反转.py
@@ -1,3 +1,11 @@
+"""
+给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+
+如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+
+link: https://leetcode-cn.com/problems/reverse-integer/
+"""
+
 class Solution:
     def reverse(self, x: int) -> int:
         x_str = str(x)
